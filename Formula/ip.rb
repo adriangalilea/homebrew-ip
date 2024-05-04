72341f6ae720fef98a09e9d831bf26f0289ac3b5
Adrian Galilea <90320947+adriangalilea@users.noreply.github.com>
2024-05-04 20:53:37 +0200

    chore: 1.0.1 formula

--- Formula/ip.rb
+++ Formula/ip.rb
@@ -1,8 +1,8 @@
 class Ip < Formula
   desc "Simplest cli tool to get your IP (local, external, gateway)"
   homepage "https://github.com/adriangalilea/homebrew-ip"
-  url "https://github.com/adriangalilea/homebrew-ip/archive/refs/tags/v1.0.tar.gz" 
-  sha256 "fc8a1b3e9c0ee97ad3409431ce1a26f90423be70ee69f62915e4ffa7f5fd6f39" 
+  url "https://github.com/adriangalilea/homebrew-ip/archive/refs/tags/v1.0.1.tar.gz" 
+  sha256 "e2fd2d182c88a4fa8faeb8628c517cedeb606692c128c26c021e2fd95e48d205" 
 
   def install
     bin.install "ip.sh" => "ip"
