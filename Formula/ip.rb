8c1f2d49360dd9bf3cb678bc50e4d3797940b29f
Adrian Galilea <90320947+adriangalilea@users.noreply.github.com>
2024-05-04 20:47:33 +0200

    fix repo name

--- Formula/ip.rb
+++ Formula/ip.rb
@@ -1,7 +1,7 @@
 class Ip < Formula
   desc "Simplest cli tool to get your IP (local, external, gateway)"
-  homepage "https://github.com/adriangalilea/ip"
-  url "https://github.com/adriangalilea/ip/archive/refs/tags/v1.0.tar.gz" 
+  homepage "https://github.com/adriangalilea/homebrew-ip"
+  url "https://github.com/adriangalilea/homebrew-ip/archive/refs/tags/v1.0.tar.gz" 
   sha256 "fc8a1b3e9c0ee97ad3409431ce1a26f90423be70ee69f62915e4ffa7f5fd6f39" 
 
   def install
