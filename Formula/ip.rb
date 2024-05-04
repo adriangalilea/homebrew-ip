79837969f7a9fc6b230ffc2ba9acf505fa473b7f
adriangalilea <adriangalilea@gmail.com>
2024-05-04 13:33:06 +0200

    chore: brew hash

--- Formula/ip.rb
+++ Formula/ip.rb
@@ -2,7 +2,7 @@ class Ip < Formula
   desc "Simplest cli tool to get your IP (local, external, gateway)"
   homepage "https://github.com/adriangalilea/ip"
   url "https://github.com/adriangalilea/ip/archive/refs/tags/v1.0.tar.gz" 
-  sha256 "bf5817134faa4b90a2aa99cdd8a61e708a680cf563f5e076df9678e36e22e622" 
+  sha256 "fc8a1b3e9c0ee97ad3409431ce1a26f90423be70ee69f62915e4ffa7f5fd6f39" 
 
   def install
     bin.install "ip.sh" => "ip"
